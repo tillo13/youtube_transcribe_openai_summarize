f791e8c0da3ea2254424cf695c81df8a4b367920
Tillo <andytillo@gmail.com>
2023-08-25 17:59:10 -0700

    add ability to pick openai model

--- openai_utils.py
+++ openai_utils.py
@@ -1,67 +1,95 @@
-# Filename: openai_utils.py
 import os
 import time
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()   # take environment variables from .env.
+load_dotenv() 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 assert OPENAI_API_KEY, "Please make sure you have set `OPENAI_API_KEY` in your .env file."
-MODEL_CHOICE = "gpt-3.5-turbo"
 
-def get_chat_response(prompt):
+# Initialise OpenAI secret key
+openai.api_key = OPENAI_API_KEY
+
+# Constants
+MODEL_CHOICES = ["gpt-3.5-turbo", "text-davinci-003"]
+DEFAULT_MODEL = "text-davinci-003"  
+#DEFAULT_MODEL = "gpt-3.5-turbo"  
+SYSTEM_MESSAGE = "You are acutely aware of recognizing names of people in videos, places they mention and things they describe.  Your job is to summarize youtube transcripts using bullet points to call out key data points"
+MAX_TOKENS_RESPONSE = 500
+MAX_TOKENS_SUMMARY = 500
+
+def get_chat_response(prompt, model_choice=DEFAULT_MODEL):
+    assert model_choice in MODEL_CHOICES, "Please make sure `model_choice` is in `MODEL_CHOICES`."
     start_time = time.time()
-    response = get_response(prompt, MODEL_CHOICE, OPENAI_API_KEY)
+    response = get_response(prompt, model_choice)
     duration = time.time() - start_time
     return response, duration
 
-def get_summary_from_transcript(transcript):
+def get_summary_from_transcript(transcript, model_choice=DEFAULT_MODEL):
+    assert model_choice in MODEL_CHOICES, "Please make sure `model_choice` is in `MODEL_CHOICES`."
     start_time = time.time()
-    summary = get_summary(transcript, MODEL_CHOICE, OPENAI_API_KEY)
+    summary = get_summary(transcript, model_choice)
     duration = time.time() - start_time
     return summary, duration
 
-def get_response(prompt, model, openai_secret_key):
-    openai.api_key = openai_secret_key
-
+def get_response(prompt, model_choice):
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": SYSTEM_MESSAGE},
         {"role": "user", "content": prompt},
     ]
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0.2,     # Lower temperature for more deterministic output.
-        max_tokens=200       # Set a limit on response length for concise response.
-    )
-    return response.choices[0].message['content'].strip()
-
-def get_summary(transcript, model, openai_secret_key):
-    openai.api_key = openai_secret_key
-    prompt = f"I have a transcript for a video that has up to 10 bullet points. Here is the transcript:\n\n{transcript}\n\nCould you provide a concise summary?"
-
-    if model == "gpt-3.5-turbo":
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ]
+    if model_choice == MODEL_CHOICES[0]:
         response = openai.ChatCompletion.create(
-            model=model,
+            model=model_choice,
             messages=messages,
-            temperature=0.2,     # Lower temperature for more deterministic output.
-            max_tokens=200       # Set a limit on response length for concise summary.
+            temperature=0.2,
+            max_tokens=MAX_TOKENS_RESPONSE
         )
-        summary = response.choices[0].message['content'].strip()
-
+        return response.choices[0].message['content'].strip()  # Moved this inside if for readability
+    elif model_choice == MODEL_CHOICES[1]:
+        prompt_string = f"{SYSTEM_MESSAGE}\n{prompt}"   # Davinci prefers single string prompts.
+        response = openai.Completion.create(
+            engine=model_choice,
+            prompt=prompt_string,
+            temperature=0.2,
+            max_tokens=MAX_TOKENS_RESPONSE
+        )
+        return response.choices[0].text.strip()  # For Davinci, use text instead
     else:
-        raise ValueError('Model not recognized. Please use "gpt-3.5-turbo".')
+        raise ValueError('Model not recognized. Please use one of the models in `MODEL_CHOICES`.')
+
+def get_summary(transcript, model_choice):
+    prompt = f"Here is a Youtube transcript for a video :\n\n{transcript}\n\nCould you provide a concise summary using bullet points?"
+    messages=[
+        {"role": "system", "content": SYSTEM_MESSAGE},
+        {"role": "user", "content": prompt},
+    ]
+  
+    if model_choice == MODEL_CHOICES[0]:
+        response = openai.ChatCompletion.create(
+            model=model_choice,
+            messages=messages,
+            temperature=0.2,
+            max_tokens=MAX_TOKENS_SUMMARY
+        )
+        summary = response.choices[0].message['content'].strip()  # Again, moved this inside if for readability
+    elif model_choice == MODEL_CHOICES[1]:
+        prompt_string = f"{SYSTEM_MESSAGE}\n{prompt}"   # Davinci prefers single string prompts.
+        response = openai.Completion.create(
+            engine=model_choice,
+            prompt=prompt_string,temperature=0.2,
+            max_tokens=MAX_TOKENS_SUMMARY
+        )
+        summary = response.choices[0].text.strip()  # For Davinci, use text
+    else:
+        raise ValueError('Model not recognized. Please use one of the models in `MODEL_CHOICES`.')
 
     return summary
 
 if __name__ == "__main__":
-    prompt = "How was your day?"
+    prompt = "What's a neat fact?"
     response, duration = get_chat_response(prompt)
     print("AI Response:")
     print(response)
     print(f"Generated in {duration} seconds.")
+    print(f"OpenAI Model used was: {DEFAULT_MODEL}")
